bf26a829c381ee82717b5e9f63a5700e95e5dfc8
petavazohi <petavazohi@mix.wvu.edu>
2020-07-22 12:47:21 -0400

    deleted the polotting scripts which moved to pyprocar, added vasp_relax and submit.py

---
+++ submit.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+import os
+import pychemia
+import shutil
+import time
+import sys
+import datetime
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-np" ,
+                    dest="np",
+                    type=int,
+                    action="store",
+                    help="Number of MPI processes for the code",
+                    default = '40')
+parser.add_argument('-i',
+                    "--input",
+                    dest='input',
+                    type=str,
+                    help='A file with list of the structures you want to be calculated')
+parser.add_argument("-xc",
+                    dest="xc",
+                    nargs='+',
+                    type=str,
+                    help='exchange correlation functional',
+                    default = ['PBE'])
+parser.add_argument("--max_jobs",
+                    dest="max_jobs",
+                    type=int,
+                    help='maximum jobs to be submited simultaniously',
+                    default=700)
+parser.add_argument("-t",
+                    "--template",
+                    type=str,
+                    help="template for the submitting job",
+                    default=None)
+
+
+args = parser.parse_args()   
+nparal = args.np
+max_jobs = args.max_jobs
+
+if args.template is None:
+    wf = open('template.pbs','w')
+    wf.write("""
+    source ~/.bashrc
+    module load atomistic/vasp/5.4.4_intel18_seq
+    vasp_relax.py -np {np} -xc {xc} --tags ENCUT 550 --structure {poscar}
+    """)
+    wf.close()
+    args.template='template.pbs'
+    
+rf = open(args.input)
+structures_init = rf.readlines()
+rf.close()
+
+to_submit=[]
+for ifile in structures_init:
+    ifile = ifile.replace('\n','')
+
+    path = ifile+'_relax'
+    if not os.path.exists(path):
+        os.mkdir(path)
+        
+    for ixc in args.xc:
+        jobname = (ixc+'-'+ifile).replace('.vasp','').replace('.ascii','')
+        path = ifile+'_relax'
+        if not os.path.exists(path+os.sep+ixc):
+            os.mkdir(path+os.sep+ixc)
+            
+        path = ifile+'_relax'+os.sep+ixc
+        if os.path.exists(path+os.sep+"relax_report.json"):
+            continue
+        else :
+            contcar = ''
+            poscar = ''
+            if os.path.exists(path+os.sep+'CONTCAR'):
+                rf = open(path+os.sep+'CONTCAR','r')
+                contcar = rf.read()
+                rf.close()
+            if os.path.exists(path+os.sep+'POSCAR'):
+                rf = open(path+os.sep+'POSCAR','r')
+                poscar = rf.read()
+                rf.close()
+            if len(contcar) != 0:
+                ifile = 'CONTCAR'
+            elif len(poscar) !=0:
+                ifile = 'POSCAR'
+            elif not os.path.exists(path+os.sep+ifile):
+                shutil.copy(ifile,path+os.sep+ifile)
+                
+            job = pychemia.runner.PBSRunner(template='template.pbs',jobname=jobname,workdir=path)
+            job.template_text = job.template_text.replace('{poscar}','"'+ifile+'"')
+            job.template_text = job.template_text.replace('{np}',str(nparal))
+            job.template_text = job.template_text.replace('{xc}',ixc)
+            job.set_pbs_params(nodes=1,
+                               ppn=nparal,
+                               walltime=[4,0,0],
+                               mail='petavazohi.hpc@gmail.com',
+                               queue='standby')
+            job.write()
+            to_submit.append(job)
+    
+counter = 1
+while True:
+    jobs_submitted = pychemia.runner.get_jobs(user=os.getenv('USER'))
+    njobs = len(jobs_submitted)
+    
+    if njobs < max_jobs and len(to_submit) != 0 :
+        job = to_submit[0]
+        current_time = datetime.datetime.now()
+        print("{: >20} {: >30} {: >20} {: >20} {: >20} {: >20}".format(counter,current_time,job.jobname,job.queue,job.nodes,job.ppn))
+        try :
+            to_submit[0].submit()
+        except :
+            
+            time.sleep(60)
+            continue
+        counter +=1 
+        del to_submit[0]
+    elif len(to_submit) == 0:
+        print("Submitted All, Exiting")
+        exit(0)
+    else :
+        # print("{} jobs submitted, sleeping for 3 minutes".format(max_jobs))
+        time.sleep(180)
---
+++ vasp_relax.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+import pychemia
+import argparse
+import json
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--structure",dest="structure",type=str,help='poscar structure that you want to run',default = 'POSCAR')
+parser.add_argument("-np" ,dest="np",type=int ,action="store", help="Number of MPI processes for the code",default = '4')
+parser.add_argument("-xc",dest="xc",type=str,help='exchange correlation functional',default = 'PBE')
+parser.add_argument('--incar_encut',dest='incar_encut',help="path to the extra variable you want to include in encut convergence",default ='none')
+parser.add_argument('--incar_kpoint',dest='incar_kpoint',help="path to the extra variable you want to include in kpoint convergence",default='none')
+parser.add_argument('--incar_relax',dest='incar_relax',help="path to the extra variable you want to include in relaxation",default='none')
+parser.add_argument('--potcar_options',dest='psp_options',nargs='+',help="The options want to be used for pseudo potentials, eg: --potcar_options O_h Cu_pv")
+parser.add_argument('--tags',dest='tags',nargs='+',help='If you want to have a tag for all calculations for example ENCUT 250', default=None)
+
+args = parser.parse_args()
+extra_vars = {}
+if args.tags is not None:
+    for itag in range(0,len(args.tags),2):
+        key = args.tags[itag]
+        value = args.tags[itag+1]
+        extra_vars[key]=value
+
+
+
+
+        
+if args.xc == 'LDA' :
+    pspdir= 'potpaw_LDA'
+elif args.xc == 'PBE':
+    pspdir = 'potpaw_PBE'
+elif args.xc == 'PBEsol':
+    pspdir = 'potpaw_PBE'
+    extra_vars['GGA']="PS"
+elif args.xc == 'AM05':
+    pspdir = 'potpaw_PBE'
+    extra_vars['GGA']="AM"
+elif args.xc == 'SCAN':
+    pspdir = 'potpaw_PBE'
+    extra_vars['METAGGA']="SCAN"
+    extra_vars['ALGO']='A'
+    extra_vars['ISMEAR']=0
+elif args.xc == 'MBJ':
+    pspdir = 'potpaw_PBE'
+    extra_vars['METAGGA']='MBJ'
+    
+psp_options = {}
+if args.psp_options:
+    for option in args.psp_options:
+        psp_options[option.split('_')[0]] = option.split('_')[1]
+
+extra_vars_encut = {}
+extra_vars_kpoint = {}
+extra_vars_relax = {}
+
+for ikey in extra_vars:
+    extra_vars_encut[ikey] = extra_vars[ikey]
+    extra_vars_kpoint[ikey] = extra_vars[ikey]
+    extra_vars_relax[ikey] = extra_vars[ikey]
+    
+if args.incar_encut != 'none' :
+    incar_encut = pychemia.code.vasp.read_incar(str(args.incar_encut))
+    extra_vars_encut = {}
+    for tag in incar_encut:
+        extra_vars_encut[tag] = incar_encut[tag]
+
+if args.incar_kpoint != 'none' :
+    incar_kpoint = pychemia.code.vasp.read_incar(str(args.incar_kpoint))
+    extra_vars_kpoint = {}
+    for tag in incar_kpoint:
+        extra_vars_kpoint[tag] = incar_kpoint[tag]
+
+if args.incar_relax != 'none' :
+    incar_relax = pychemia.code.vasp.read_incar(str(args.incar_relax))
+    extra_vars_relax = {}
+    for tag in incar_relax:
+        extra_vars_relax[tag] = incar_relax[tag]
+
+
+
+if 'ascii' in args.structure:
+    st = pychemia.io.ascii.load(args.structure)
+else : 
+    st = pychemia.code.vasp.read_poscar(args.structure)
+
+
+
+if 'ENCUT' in extra_vars:
+    encut = int(extra_vars['ENCUT'])
+    del extra_vars['ENCUT']
+    del extra_vars_kpoint['ENCUT']
+    del extra_vars_relax['ENCUT']
+                
+elif not os.path.exists('encut_report.json'):
+    encut_conv = pychemia.code.vasp.task.ConvergenceCutOffEnergy(structure=st,
+                                                                 workdir='.',
+                                                                 executable='vasp_std',
+                                                                 pspdir=pspdir,
+                                                                 extra_vars=extra_vars_encut,
+                                                                 energy_tolerance=1E-3,
+                                                                 psp_options=psp_options)
+    encut_conv.run(nparal=args.np)
+    encut_conv.save('encut_report.json')
+    if not encut_conv.success:
+        print("ENCUT convergence unsuccessful")
+        exit()
+    encut = encut_conv.best_encut
+else :
+    rf = open('encut_report.json','r')
+    encut_data =json.load(rf)
+    rf.close()
+    encut = encut_data['output']['best_encut']
+
+    
+if not os.path.exists('kpoint_report.json'):
+    kpt_conv = pychemia.code.vasp.task.ConvergenceKPointGrid(structure=st,
+                                                             workdir='.',
+                                                             executable='vasp_std',
+                                                             pspdir=pspdir,
+                                                             extra_vars=extra_vars_kpoint,
+                                                             energy_tolerance=1E-3,
+                                                             psp_options=psp_options)
+    kpt_conv.run(args.np)
+    kpt_conv.save('kpoint_report.json')
+    if not kpt_conv.success:
+        print('Kpoint convergence unsuccessful')
+        exit()
+    kp = kpt_conv.best_kpoints
+    kgrid = kpt_conv.best_kpoints.grid
+
+else :
+    rf = open('kpoint_report.json','r')
+    kpoint_data = json.load(rf)
+    rf.close()
+    kgrid = kpoint_data['output']['best_kp_grid']
+
+relax = pychemia.code.vasp.task.IonRelaxation(structure=st,
+                                              workdir='.',
+                                              target_forces=1e-3,
+                                              executable='vasp_std',
+                                              encut=encut,
+                                              kp_grid=kgrid,
+                                              pspdir=pspdir,
+                                              max_calls=20,
+                                              extra_vars=extra_vars_relax,
+                                              psp_options=psp_options)
+relax.run(args.np)
+relax.save('relax_report.json')
